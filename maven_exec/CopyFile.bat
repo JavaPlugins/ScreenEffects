4b6d8473e555ecc95a9623c2e1406bf3525eb9d5
LoneDev <27242001+LoneDev6@users.noreply.github.com>
2025-06-02 11:59:48 +0200

    Removed LoneLibs requirement

--- maven_exec/CopyFile.bat
+++ maven_exec/CopyFile.bat
@@ -1,2 +1,1 @@
-echo f | xcopy C:\Progetti\Minecraft\Spigot\ScreenEffects\target\ScreenEffects-1.0-SNAPSHOT.jar C:\Progetti\Minecraft\TestServer\1.20.6\plugins\ScreenEffects.jar /Y
-echo f | xcopy C:\Progetti\Minecraft\Spigot\ScreenEffects\target\ScreenEffects-1.0-SNAPSHOT.jar C:\Progetti\Minecraft\TestServer\1.21.4\plugins\ScreenEffects.jar /Y
+echo f | xcopy C:\Progetti\Minecraft\Spigot\ScreenEffects\target\ScreenEffects-1.0-SNAPSHOT.jar C:\Progetti\Minecraft\TestServer\1.21.5\plugins\ScreenEffects.jar /Y
